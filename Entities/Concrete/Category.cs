32dc6925af402dd129b09da867e56adb50fea254
krack16 <47774247+krack16@users.noreply.github.com>
2021-01-30 23:02:25 +0300

    300121

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+   public class Category:IEntity
+    {
+        //Çıplak Class Kalmasın: Interface almayan çıplak kalır.
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
