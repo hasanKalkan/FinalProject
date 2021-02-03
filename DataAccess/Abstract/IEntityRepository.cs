fc5368a00418706f5617465819d7f2b7648cbaa2
krack16 <47774247+krack16@users.noreply.github.com>
2021-02-03 22:14:24 +0300

    3221

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,14 +1,19 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T> //varlık deposu
+    //generic constraint: genel kısıtlama
+    //class: referans tip olabilir.
+    public interface IEntityRepository<T> where T:class,IEntity //varlık deposu: IEntity ile alakalı olanlar
     {
-        List<T> GetAll(Expression<Func<T, bool>> filter = null); //ayrı ayrı getirmeye gerek kalmayacak, filter=null. filtre vermeyebilirsin, verilmezse null olur.
+        List<T> GetAll(Expression<Func<T, bool>> filter = null); 
+        //ayrı ayrı getirmeye gerek kalmayacak, filter=null. filtre vermeyebilirsin, verilmezse null olur.
         //p=>p.CategoryId=2 gibi filtreler yazmamızı sağlar.
+        
         T Get();
         void Add(T entity);
         void Update(T entity);
