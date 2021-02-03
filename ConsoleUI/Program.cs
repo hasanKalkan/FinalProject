2a50b3366853c81d1a43be72b3e6703bcf9a4328
krack16 <47774247+krack16@users.noreply.github.com>
2021-02-04 00:11:05 +0300

    4221

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,9 +11,11 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+          //  ProductManager productManager = new ProductManager(new InMemoryProductDal());
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetAll())
-            {
+          //  foreach (var product in productManager.GetAllByCategoryId(2))
+            foreach (var product in productManager.GetByUnitPrice(20,100))
+                {
                 Console.WriteLine(product.ProductName);
             }
         }
