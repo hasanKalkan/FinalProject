98a017f2e633a730b3b857cd9e4cdcae343d0e3c
krack16 <47774247+krack16@users.noreply.github.com>
2021-02-03 23:36:44 +0300

    3221

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -40,7 +41,12 @@ namespace DataAccess.Concrete.EntityFramework
 
         public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context=new NorthwindContext())
+            {
+                return filter == null  //? ternary operatörü
+                    ? context.Set<Product>().ToList()  //filtre null ise
+                    : context.Set<Product>().Where(filter).ToList(); //filtre null değilse
+            }
         }
 
         public void Update(Product entity)
