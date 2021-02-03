2176669e7b2fcf25acca9f4ba6645d92fc219c9e
krack16 <47774247+krack16@users.noreply.github.com>
2021-02-03 21:49:58 +0300

    3221

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> //varlık deposu
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null); //ayrı ayrı getirmeye gerek kalmayacak, filter=null. filtre vermeyebilirsin, verilmezse null olur.
+        //p=>p.CategoryId=2 gibi filtreler yazmamızı sağlar.
+        T Get();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
