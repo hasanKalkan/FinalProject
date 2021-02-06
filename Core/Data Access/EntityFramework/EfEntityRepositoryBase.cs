56dc71edc0a9a7ae0a5b281cdb5e566dbd97764d
krack16 <47774247+krack16@users.noreply.github.com>
2021-02-06 22:22:47 +0300

    6221

---
+++ Core/Data Access/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Data_Access.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext>
+        where TEntity:class,IEntity, new()
+        where TContext:DbContext
+    {
+    }
+}
