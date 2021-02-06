09febd736d695245da933acf861fe772e3419629
krack16 <47774247+krack16@users.noreply.github.com>
2021-02-06 22:34:36 +0300

    6221

--- Core/Data Access/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/Data Access/EntityFramework/EfEntityRepositoryBase.cs
@@ -2,13 +2,67 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
+
 
 namespace Core.Data_Access.EntityFramework
 {
-    public class EfEntityRepositoryBase<TEntity, TContext>
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
         where TEntity:class,IEntity, new()
-        where TContext:DbContext
+        where TContext:DbContext,new()
     {
+        //Entity Framework: ORM veritabanındaki tabloyu class gibi ilişkilendirir.
+        //NuGet : kodların ortak kullanıldığı alan
+        public void Add(TEntity entity)
+        {
+            //Using: IDisposable pattern implementation of c#
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null  //? ternary operatörü
+                    ? context.Set<TEntity>().ToList()  //filtre null ise
+                    : context.Set<TEntity>().Where(filter).ToList(); //filtre null değilse
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
     }
 }
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -11,56 +11,6 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfProductDal : IProductDal
     {
-        //Entity Framework: ORM veritabanındaki tabloyu class gibi ilişkilendirir.
-        //NuGet : kodların ortak kullanıldığı alan
-        public void Add(Product entity)
-        {
-            //Using: IDisposable pattern implementation of c#
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return filter == null  //? ternary operatörü
-                    ? context.Set<Product>().ToList()  //filtre null ise
-                    : context.Set<Product>().Where(filter).ToList(); //filtre null değilse
-            }
-        }
-
-        public void Update(Product entity)
-        {
-
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
+       
     }
 }
