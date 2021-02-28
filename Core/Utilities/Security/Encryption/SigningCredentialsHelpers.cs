7c897c86b24e7a6f154d39f092704b2d44ddf80f
krack16 <47774247+krack16@users.noreply.github.com>
2021-02-28 09:55:11 +0300

    28221

---
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelpers.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Encryption
+{
+    public class SigningCredentialsHelpers
+    {
+        public static SigningCredentials CreteSigningCredentials(SecurityKey securityKey)
+        {
+            return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+        }
+    }
+}
